17cf89d09b83e6938d11bf1934c01c0b5d5f95f8
Vasant Saraswat <vasant.saraswat@gmail.com>
2021-09-17 02:37:44 +0530

    Changes updated

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,3 +1,5 @@
+function important(){
+
 let container = document.querySelector("main")
 let rootTag =document.querySelector("ul")
 let search = document.querySelector(".search")
@@ -5,13 +7,13 @@ let search = document.querySelector(".search")
 let allPeople = got.houses.reduce((acc,cv)=>{
     acc =acc.concat(cv.people);
     return acc;
-},[])
+},[]);
 
 let allTags = got.houses.map((house)=>house.name);
 
 let activeHouse = "";
-function card(data = []){
-    container.innerText=""
+function card(data){
+          container.innerText="";
           data.forEach((peoples)=>{
               let div =document.createElement("div");
               div.classList.add("box");
@@ -32,11 +34,14 @@ function card(data = []){
       
 }
       
-function createTagUI(tags=[]){
+function createTagUI(tags){
           rootTag.innerText ="";
           tags.forEach((tag)=>{
-              let li =document.createElement("li")
+              let li =document.createElement("button")
               li.innerText=tag;
+              li.style.color = "pink";
+              li.style.borderRadius = "40px";
+              li.style.backgroundColor = "black"
               li.addEventListener("click",()=>{
                   activeHouse = tag;
                   let peopleOfGotHouses = got.houses.find((house)=> house.name === tag ).people || [];
@@ -44,12 +49,16 @@ function createTagUI(tags=[]){
               })
               rootTag.append(li);
           })
-      }
-function handleSearch(event){
-let text = event.target.value;
-let filteredPeople =allPeople.filter((p)=>p.name.toLowerCase().includes(text.toLowerCase()));
-card(filteredPeople);
 }
-      search.addEventListener("input",handleSearch);
-      card(allPeople);
-      createTagUI(allTags);
+
+function handleSearch(event){
+    let text = event.target.value;
+    let filteredPeople =allPeople.filter((p)=>p.name.toLowerCase().includes(text.toLowerCase()));
+    card(filteredPeople);
+    }
+    search.addEventListener("input",handleSearch);
+    card(allPeople);
+    createTagUI(allTags);
+}
+
+important();
